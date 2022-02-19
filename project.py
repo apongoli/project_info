c12228382c41dd80faa5a71f91dab7bff61a4ef4
apongoli <pongolialessandro@outlook.be>
2022-02-19 15:11:21 +0100

    Le debut de la fonction init_board

--- project.py
+++ project.py
@@ -14,11 +14,71 @@ def clear():
 
 
 # Initialise Game Board
-col = 20
-row = 20
-board = [[' ' for i in range(col)] for i in range(row)]
 
-clear()
+def init_board(col=20, row=20):
+    board = [[' ' for i in range(col)] for i in range(row)]
+
+    board[1][1] = "\u2655"
+    board[0][0] = "\u2656"
+    board[0][1] = "\u2658"
+    board[1][0] = "\u2658"
+    board[0][2] = "\u2658"
+    board[1][2] = "\u2658"
+    board[2][2] = "\u2658"
+    board[2][1] = "\u2658"
+    board[2][0] = "\u2658"
+
+    board[18][18] = "\u265B"
+    board[19][19] = "\u265C"
+    board[19][18] = "\u265E"
+    board[18][19] = "\u265E"
+    board[19][17] = "\u265E"
+    board[18][17] = "\u265E"
+    board[17][17] = "\u265E"
+    board[17][18] = "\u265E"
+    board[17][19] = "\u265E"
+
+    board[3][3] = "X"
+    board[3][4] = "X"
+    board[4][3] = "X"
+    board[4][4] = "X"
+    board[15][15] = "X"
+    board[15][16] = "X"
+    board[16][15] = "X"
+    board[16][16] = "X"
+    board[0][3] = "\u25CB"
+    board[0][4] = "\u25CB"
+    board[19][15] = "\u25CB"
+    board[19][16] = "\u25CB"
+    board[3][0] = "\u25B3"
+    board[4][0] = "\u25B3"
+    board[15][19] = "\u25B3"
+    board[16][19] = "\u25B3"
+    board[4][6] = "\u25B3"
+    board[15][13] = "\u25B3"
+    board[6][4] = "\u2606"
+    board[13][15] = "\u2606"
+
+    clear()
+    for x in range(0, row):
+        for y in range(0, col):
+            if (x + y) % 2 == 0:
+                if y < col - 1:
+                    print(colored.fg('blue') + colored.bg('white') + " " + board[x][y], end=" " + colored.attr('reset'))
+                else:
+                    print(colored.fg('blue') + colored.bg('white') + " " + board[x][y], end=" " + colored.attr('reset'))
+                    print("")
+            else:
+                if y < col - 1:
+                    print(colored.fg('yellow') + colored.bg('black') + " " + board[x][y],
+                          end=" " + colored.attr('reset'))
+                else:
+                    print(colored.fg('yellow') + colored.bg('black') + " " + board[x][y],
+                          end=" " + colored.attr('reset'))
+                    print("")
+
+
+init_board()
 """ Different code for symbols
 print("\u2655")  # white Alpha
 print("\u2656")  # white Omega
@@ -36,59 +96,3 @@ print("X")  # Berries
 print("\u25B3")  # Mice
 print("\u2606")  # Deer
 """
-
-board[1][1] = "\u2655"
-board[0][0] = "\u2656"
-board[0][1] = "\u2658"
-board[1][0] = "\u2658"
-board[0][2] = "\u2658"
-board[1][2] = "\u2658"
-board[2][2] = "\u2658"
-board[2][1] = "\u2658"
-board[2][0] = "\u2658"
-
-board[18][18] = "\u265B"
-board[19][19] = "\u265C"
-board[19][18] = "\u265E"
-board[18][19] = "\u265E"
-board[19][17] = "\u265E"
-board[18][17] = "\u265E"
-board[17][17] = "\u265E"
-board[17][18] = "\u265E"
-board[17][19] = "\u265E"
-
-board[3][3] = "X"
-board[3][4] = "X"
-board[4][3] = "X"
-board[4][4] = "X"
-board[15][15] = "X"
-board[15][16] = "X"
-board[16][15] = "X"
-board[16][16] = "X"
-board[0][3] = "\u25CB"
-board[0][4] = "\u25CB"
-board[19][15] = "\u25CB"
-board[19][16] = "\u25CB"
-board[3][0] = "\u25B3"
-board[4][0] = "\u25B3"
-board[15][19] = "\u25B3"
-board[16][19] = "\u25B3"
-board[4][6] = "\u25B3"
-board[15][13] = "\u25B3"
-board[6][4] = "\u2606"
-board[13][15] = "\u2606"
-
-for x in range(0, row):
-    for y in range(0, col):
-        if (x + y) % 2 == 0:
-            if y < col - 1:
-                print(colored.fg('blue') + colored.bg('white') + " " + board[x][y], end=" " + colored.attr('reset'))
-            else:
-                print(colored.fg('blue') + colored.bg('white') + " " + board[x][y], end=" " + colored.attr('reset'))
-                print("")
-        else:
-            if y < col - 1:
-                print(colored.fg('yellow') + colored.bg('black') + " " + board[x][y], end=" " + colored.attr('reset'))
-            else:
-                print(colored.fg('yellow') + colored.bg('black') + " " + board[x][y], end=" " + colored.attr('reset'))
-                print("")
